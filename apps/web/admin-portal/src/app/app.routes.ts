5a79aa253146277e2ae547f343b2d25fd0828cbd
Puneet Sharma <psharma@gftpay.com>
2026-03-06 20:39:53 +0530

    feat: add 'web-admin' angular app login page

---
+++ apps/web/admin-portal/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login/login-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginPageComponent,
+    title: 'Whizard Admin Login'
+  }
+];
